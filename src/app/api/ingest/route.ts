5d6f2fd49610b6f7ff5b817413d5d9fd745399e4
Gianluca Venturini <gianluca.91@gmail.com>
2024-03-02 12:52:55 -0800

    Fix IngestQuestion interface

--- src/app/api/ingest/route.ts
+++ src/app/api/ingest/route.ts
@@ -5,13 +5,13 @@ import { setValue } from '../clients/redis';
 import { QUESTION_PREFIX } from '@/constants';
 
 interface IngestQuestion {
-    text: string;
+    questionText: string;
 }
 
 export async function POST(request: Request) {
     const requestData: IngestQuestion = await request.json();
     console.log('Received a new question', requestData);
-    setValue(`${QUESTION_PREFIX}${uuid()}`, requestData.text);
+    setValue(`${QUESTION_PREFIX}${uuid()}`, requestData.questionText);
     const body = { message: 'Your questions have been received, we will answer it shortly. Thank you!', requestData };
     return new Response(JSON.stringify(body), {
         status: 201,
