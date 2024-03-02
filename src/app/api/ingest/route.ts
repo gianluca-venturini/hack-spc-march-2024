245608f0bd9003c376531d1f8fe0203c7b8b194b
Gianluca Venturini <gianluca.91@gmail.com>
2024-03-02 10:44:19 -0800

    Ingest endpoint

--- src/app/api/ingest/route.ts
+++ src/app/api/ingest/route.ts
@@ -2,5 +2,13 @@
 export const dynamic = 'force-dynamic' // defaults to auto
 
 export async function POST(request: Request) {
-    return new Response('Your questions have been received, we will answer it shortly. Thankyou!', { status: 200 });
+    const requestData = await request.json();
+    console.log('Received a new question', requestData);
+    const body = { message: 'Your questions have been received, we will answer it shortly. Thank you!', requestData };
+    return new Response(JSON.stringify(body), {
+        status: 200,
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
 }
