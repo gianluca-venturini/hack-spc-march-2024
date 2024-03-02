00722b5073c314b1a035f5e7bd8588d8ed9503f0
Divyanshu Goyal <divyanshu.g25@gmail.com>
2024-03-02 09:57:53 -0800

    changing accept message and adding black formatting
    Signed-off-by: Divyanshu Goyal <divyanshu.g25@gmail.com>

--- hello.py
+++ hello.py
@@ -7,11 +7,8 @@ load_dotenv()  # This loads the environment variables from .env
 
 client = OpenAI()
 
-audio_file= open("test.mp3", "rb")
+audio_file = open("test.mp3", "rb")
 
-transcription = client.audio.transcriptions.create(
-  model="whisper-1", 
-  file=audio_file
-)
+transcription = client.audio.transcriptions.create(model="whisper-1", file=audio_file)
 
 print(transcription.text)
--- src/app/api/ingest/route.ts
+++ src/app/api/ingest/route.ts
@@ -1,6 +1,6 @@
 
 export const dynamic = 'force-dynamic' // defaults to auto
 
-export async function GET(request: Request) {
-    return new Response('Lololal', { status: 200 });
+export async function POST(request: Request) {
+    return new Response('Your questions have been received, we will answer it shortly. Thankyou!', { status: 200 });
 }
