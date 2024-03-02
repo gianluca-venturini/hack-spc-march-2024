182f6450e88b865caf9334feebecc010d1527c9c
Gianluca Venturini <gianluca.91@gmail.com>
2024-03-02 10:48:37 -0800

    Add interface for ingested questions

--- src/app/api/ingest/route.ts
+++ src/app/api/ingest/route.ts
@@ -1,8 +1,12 @@
 
 export const dynamic = 'force-dynamic' // defaults to auto
 
+interface IngestQuestion {
+    text: string;
+}
+
 export async function POST(request: Request) {
-    const requestData = await request.json();
+    const requestData: IngestQuestion = await request.json();
     console.log('Received a new question', requestData);
     const body = { message: 'Your questions have been received, we will answer it shortly. Thank you!', requestData };
     return new Response(JSON.stringify(body), {
