5bc637eee5ac0876e6ec3eb0a3c82bdb292e54ac
Gianluca Venturini <gianluca.91@gmail.com>
2024-03-03 00:31:54 -0800

    Copy change

--- src/app/ask/page.tsx
+++ src/app/ask/page.tsx
@@ -5,7 +5,7 @@ export default async function Policy() {
         className="flex items-center justify-center border border-white text-white bg-transparent h-12 px-20 rounded-full shadow-lg transition-all duration-150 ease-in-out transform hover:-translate-y-1 focus:outline-none focus:ring-4"
         href="https://chat.openai.com/g/g-5ELcqgUiA-lecturemoderatorgpt"
       >
-        Start asking questions
+        Ask questions
     </a>
     </main>
   );
